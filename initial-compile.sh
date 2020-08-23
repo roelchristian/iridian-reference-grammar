d892aeeb064a9f78ebc903cb45585fe5609df20d
roelchristian <yambao.roelchristian@gmail.com>
2020-08-24 00:37:30 +0800

    Update compile engine

--- initial-compile.sh
+++ initial-compile.sh
@@ -3,6 +3,6 @@
 # Run this command first to grant permission
 # chmod +x initial-compile.sh
 
-xelatex iridian-grammar.tex
+lualatex iridian-grammar.tex
 biber iridian-grammar
-xelatex iridian-grammar.tex
+lualatex iridian-grammar.tex
